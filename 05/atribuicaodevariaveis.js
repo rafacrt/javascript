3ff523409f8a097d833c0bc54c5f9a3369bcbf4a
Rafael Medeiros <banda_c4_rafa@yahoo.com.br>
2022-05-17 20:19:08 -0300

    alt

---
+++ 05/atribuicaodevariaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const nome = "Ricardo";
+const sobrenome = "Bugan";
+
+
+console.log(`Meu nome é ${nome}`)
---
+++ 10/3-operacoesAritiméticas.js
@@ -0,0 +1,4 @@
+console.log("Operacões Aritiméticas");
+
+console.log(2+2);
+console.log((10 + 8) * 2);
